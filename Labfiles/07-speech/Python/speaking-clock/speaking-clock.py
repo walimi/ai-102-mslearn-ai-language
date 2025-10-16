9dbf462e6cf7bf19d39752837c6613a4a438bcec
Wahid Alimi <wahid.alimi@gmail.com>
2025-10-15 23:14:19 -0400

    feat: Enable real-time speech processing in speaking-clock
    - Switch audio input from file (time.wav) to live microphone
    - Replace file output with direct speaker playback
    - Change voice from Turkish (tr-TR-EmelNeural) to English (en-GB-RyanNeural)
    - Simplify speech synthesis by removing SSML markup and custom messages
    - Add user prompt 'Speak now...' for better interaction
    - Configure Azure Speech service with live audio I/O

--- Labfiles/07-speech/Python/speaking-clock/speaking-clock.py
+++ Labfiles/07-speech/Python/speaking-clock/speaking-clock.py
@@ -35,19 +35,18 @@ def TranscribeCommand():
     command = ''
 
     # Configure speech recognition
-    current_dir = os.getcwd()
-    audioFile = current_dir + '/time.wav'
-    audio_config = speech_sdk.AudioConfig(filename=audioFile)
+    audio_config = speech_sdk.AudioConfig(use_default_microphone=True)
     speech_recognizer = speech_sdk.SpeechRecognizer(speech_config, audio_config)
+    print('Speak now...')
 
 
     # Process speech input
-    print("Listening...")
     speech = speech_recognizer.recognize_once_async().get()
     if speech.reason == speech_sdk.ResultReason.RecognizedSpeech:
         command = speech.text
         print(command)
     else:
+        print(speech.reason)
         if speech.reason == speech_sdk.ResultReason.Canceled:
             cancellation = speech.cancellation_details
             print(cancellation.reason)
@@ -64,26 +63,15 @@ def TellTime():
 
 
     # Configure speech synthesis
-    output_file = "output.wav"
-    speech_config.speech_synthesis_voice_name = "tr-TR-EmelNeural"
-    audio_config = speech_sdk.audio.AudioConfig(filename=output_file)
+    speech_config.speech_synthesis_voice_name = "en-GB-RyanNeural"
+    audio_config = speech_sdk.audio.AudioOutputConfig(use_default_speaker=True)
     speech_synthesizer = speech_sdk.SpeechSynthesizer(speech_config, audio_config)
 
 
     # Synthesize spoken output
-    responseSsml = " \
-    <speak version='1.0' xmlns='http://www.w3.org/2001/10/synthesis' xml:lang='en-US'> \
-        <voice name='tr-TR-EmelNeural'> \
-            {} \
-            <break strength='weak'/> \
-            Time to end this lab! \
-        </voice> \
-    </speak>".format(response_text)
-    speak = speech_synthesizer.speak_ssml_async(responseSsml).get()
+    speak = speech_synthesizer.speak_text_async(response_text).get()
     if speak.reason != speech_sdk.ResultReason.SynthesizingAudioCompleted:
         print(speak.reason)
-    else:
-        print("Spoken output saved in " + output_file)
 
 
 if __name__ == "__main__":
